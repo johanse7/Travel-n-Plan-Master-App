71398b5d421f3d530183d5e5416522443cb47a0e
unknown <johanrodriguez_14@hotmail.com>
2019-09-23 08:38:28 -0500

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/styles/components/Header.scss';
+
+const Header = () => (
+  <header className='header'>
+    <h1>Travel plan master</h1>
+  </header>
+);
+
+export default Header;
--- src/containers/TravelApp.jsx
+++ src/containers/TravelApp.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Layout from '../components/Layout';
+import Header from '../components/Header';
 import Register from '../components/Register';
 import '../assets/styles/TravelApp.scss';
 
 const TravelApp = () => (
-
-  <Register />
+  <Layout>
+    <Header />
+    <Register />
+  </Layout>
 );
 
 export default TravelApp;
