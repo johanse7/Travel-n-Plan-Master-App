9a5903f7ee9be3b86e70e0fdd32cddf14c2d98fd
unknown <johanrodriguez_14@hotmail.com>
2019-09-23 17:40:32 -0500

    add swich in router

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
-import Register from '../components/Register';
+import Register from './Register';
 import '../assets/styles/TravelApp.scss';
 
-const TravelApp = () => (
+const Home = () => (
   <Layout>
     <Header />
     <Register />
   </Layout>
 );
 
-export default TravelApp;
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TravelApp from './containers/TravelApp';
+import TravelApp from './routes/TravelApp';
 
 ReactDOM.render(<TravelApp />, document.getElementById('TravelMasterApp'));
