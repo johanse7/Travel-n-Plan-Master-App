a1a55244828073922ed71962a8c6ac6f582b2623
unknown <johanrodriguez_14@hotmail.com>
2019-09-23 21:06:40 -0500

    add Route

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Header from './Header';
 
 const Layout = ({ children }) => (
   <section>
+    <Header />
     {children}
   </section>
 );
--- src/routes/TravelApp.js
+++ src/routes/TravelApp.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
+import Login from '../containers/login';
+import Register from '../containers/Register';
+import Layout from '../components/Layout';
 
 const TravelApp = () => (
-    <BrowserRouter >
-        <Route exact path ='/' component={Home} />
+    <BrowserRouter>
+        <Layout>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+            </Switch>
+        </Layout>
     </BrowserRouter>
 );
 
