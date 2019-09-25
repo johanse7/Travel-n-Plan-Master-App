4fa7712d8461412ab85133e92ca7b5130d6efebc
unknown <johanrodriguez_14@hotmail.com>
2019-09-24 20:34:10 -0500

    add Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
 import TravelApp from './routes/TravelApp';
 
-ReactDOM.render(<TravelApp />, document.getElementById('TravelMasterApp'));
+ReactDOM.render(
+//   <Provider>
+    <TravelApp />,
+//   </Provider>,
+  document.getElementById('TravelMasterApp')
+);
