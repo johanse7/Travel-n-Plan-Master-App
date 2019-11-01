945105043965246957b8dcaf97379a4cf33490c9
Sebastian Rodriguez Sanchez <xcbsrodr@intcomex.com>
2019-11-01 17:41:02 -0500

    react thuk fetch api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware  } from 'redux';
+import { createStore, applyMiddleware , compose } from 'redux';
 import reducer from './reducers/index';
 import TravelApp from './routes/TravelApp';
 
@@ -126,9 +126,10 @@ const intialState = {
 
   ]
 };
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
 const store = createStore(reducer,
   intialState,
-  applyMiddleware(thunk));
+  compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
 
 ReactDOM.render(
   <Provider store={store}>
